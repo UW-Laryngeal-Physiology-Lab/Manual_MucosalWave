9c950905fa439b3d7119c29fde2921232902da48
scholp94 <scholp@uwalumni.com>
2021-04-13 15:06:33 -0500

    removing to reorganize

--- functions/allEdgesSelected.m
+++
@@ -1,5 +0,0 @@
-function done = allEdgesSelected(ruEdge, rlEdge, luEdge, llEdge)
-
-done = false;
-
-if (any(ruEdge) & any(rlEdge) & any(luEdge) & any(llEdge)), done = true; end
--- functions/convertAmpToRealUnits.m
+++
@@ -1,5 +0,0 @@
-% --- Converts a radian frequency to actual frequency (Hz)
-function ampWithUnits = convertAmpToRealUnits(amp, unitsStruct)
-
-UNITS_CONVERSION_INDEX = 3;
-ampWithUnits = amp .* unitsStruct{UNITS_CONVERSION_INDEX};
