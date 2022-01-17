d8232ea7a1e43f1372a99cfb74918bc5635fd71c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-17 10:11:17 +0800

    Delete MucosalWave_v2015 directory

--- MucosalWave_v2015/functions/getAmplitudeResults.m
+++
@@ -1,9 +0,0 @@
-function ampResults = getAmplitudeResults(cfitObject, order)
-% This function sets all the amp result labels in the Tune GUI
-
-coeff = coeffvalues(cfitObject);
-ampResults = [];
-for k = 1:order
-    [amp, phase] = convertSineFormat(coeff(2*k), coeff(2*k+1));
-    ampResults(end+1) = amp;
-end
--- MucosalWave_v2015/functions/getSelectedEdges.m
+++
@@ -1,3 +0,0 @@
-function edges = getSelectedEdges(handles)
-
-edges = {handles.ruEdges handles.rlEdges handles.luEdges handles.llEdges};
